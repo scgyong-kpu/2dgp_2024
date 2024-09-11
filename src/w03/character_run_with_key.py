0928b8cc18970b2eb5e9ad483fd7ab33db019ba0
Kiyong Kim <scgyong@tukorea.ac.kr>
2024-09-12 00:42:38 +0900

    마우스로는 target x,y 만 설정하고 dx,dy 를 설정함으로써 그곳을 향해 움직이도록 한다.

--- src/w03/character_run_with_key.py
+++ src/w03/character_run_with_key.py
@@ -3,7 +3,7 @@ from pico2d import *
 def handle_events():
   global running
   global dx
-  global x, y, flip
+  global tx, ty
 
   for e in get_events():
     if e.type == SDL_QUIT:
@@ -23,11 +23,7 @@ def handle_events():
       elif e.key == SDLK_RIGHT:
         dx -= 1
     elif e.type == SDL_MOUSEMOTION:
-      mdx = e.x - x
-      if mdx < 0: flip = 'h'
-      if mdx > 0: flip = ''
-
-      x, y = e.x, get_canvas_height() - e.y
+      tx, ty = e.x, get_canvas_height() - e.y
 
 
 open_canvas()
@@ -37,9 +33,9 @@ character = load_image('run_animation.png')
 frame_index = 0
 
 x, y = get_canvas_width() // 2, get_canvas_height() // 2
-hide_cursor()
+tx, ty = -1, -1
 
-dx = 0
+dx, dy = 0, 0
 running = True
 flip = ''
 
@@ -48,7 +44,24 @@ while running:
   grass.draw(400, 30)
   character.clip_composite_draw(100 * frame_index, 0, 100, 100, 0, flip, x, y, 100, 100)
   frame_index = (frame_index + 1) % 8
+  if tx >= 0:
+    if tx < x: 
+      dx = -1
+    elif tx > x: 
+      dx = 1
+    else:
+      dx = 0
+      tx = -1
+  if ty >= 0:
+    if ty < y: 
+      dy = -1
+    elif ty > y: 
+      dy = 1
+    else:
+      dy = 0
+      ty = -1
   x += dx
+  y += dy
   if dx < 0: flip = 'h'
   if dx > 0: flip = ''
   print(f'{x=} {dx=:2} {flip=}')
