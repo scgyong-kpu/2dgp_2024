79b47005c31de57911dbb3b9e0936b685a1848a0
Kiyong Kim <scgyong@tukorea.ac.kr>
2024-09-11 23:02:18 +0900

    첫번째 프레임만 잘라서 보여달라

--- src/w03/character_move.py
+++ src/w03/character_move.py
@@ -8,7 +8,7 @@ character = load_image('run_animation.png')
 for x in range(0, 800):
   clear_canvas()
   grass.draw(400, 30)
-  character.draw(x, 90)
+  character.clip_draw(50, 50, 50, 50, x, 90)
   update_canvas()
   delay(0.01)
 
