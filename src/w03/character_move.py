b53b1d67d56b4b6366d16a483be7b0756a0959d5
Kiyong Kim <scgyong@tukorea.ac.kr>
2024-09-11 23:13:28 +0900

    2배 크기로 그려보기.

--- src/w03/character_move.py
+++ src/w03/character_move.py
@@ -9,7 +9,7 @@ frame_index = 0
 for x in range(0, 800, 5):
   clear_canvas()
   grass.draw(400, 30)
-  character.clip_draw(100 * frame_index, 0, 100, 100, x, 90)
+  character.clip_draw(100 * frame_index, 0, 100, 100, x, 90, 200, 200)
   frame_index = (frame_index + 1) % 8
   update_canvas()
   delay(0.01)
