1e9b6549efb874f0cbff4c23b0572ceb4d312e37
Kiyong Kim <scgyong@tukorea.ac.kr>
2024-09-12 00:15:45 +0900

    키가 눌린 상태를 추적한다.

--- src/w03/character_run_with_key.py
+++ src/w03/character_run_with_key.py
@@ -2,7 +2,7 @@ from pico2d import *
 
 def handle_events():
   global running
-  global x
+  global dx
   for e in get_events():
     if e.type == SDL_QUIT:
       running = False
@@ -10,9 +10,16 @@ def handle_events():
       if e.key == SDLK_ESCAPE:
         running = False
       elif e.key == SDLK_LEFT:
-        x -= 10
+        dx -= 1
       elif e.key == SDLK_RIGHT:
-        x += 10
+        dx += 1
+    elif e.type == SDL_KEYUP:
+      if e.key == SDLK_ESCAPE:
+        running = False
+      elif e.key == SDLK_LEFT:
+        dx += 1
+      elif e.key == SDLK_RIGHT:
+        dx -= 1
 
 
 open_canvas()
@@ -22,6 +29,7 @@ character = load_image('run_animation.png')
 frame_index = 0
 
 x = 800 // 2
+dx = 0
 running = True
 
 while running:
@@ -29,6 +37,8 @@ while running:
   grass.draw(400, 30)
   character.clip_draw(100 * frame_index, 0, 100, 100, x, 90, 100, 100)
   frame_index = (frame_index + 1) % 8
+  x += dx
+  print(f'{x=} {dx=}')
   update_canvas()
 
   handle_events()
