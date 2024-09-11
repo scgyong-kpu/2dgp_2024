f133fa02f78a5be3c94829868f7ed7f777442c53
Kiyong Kim <scgyong@tukorea.ac.kr>
2024-09-11 23:06:00 +0900

    frame_index 를 변경해 가면서 애니메이션을 진행한다.

--- src/w03/character_move.py
+++ src/w03/character_move.py
@@ -4,11 +4,14 @@ open_canvas()
 
 grass = load_image('grass.png')
 character = load_image('run_animation.png')
+frame_index = 0
 
-for x in range(0, 800):
+for x in range(0, 800, 5):
   clear_canvas()
   grass.draw(400, 30)
-  character.clip_draw(50, 50, 50, 50, x, 90)
+  character.clip_draw(100 * frame_index, 0, 100, 100, x, 90)
+  frame_index += 1
+  if frame_index == 8: frame_index = 0
   update_canvas()
   delay(0.01)
 
