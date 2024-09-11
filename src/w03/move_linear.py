b4a0a2552a8edc268d79d88f810e4c793a9c9504
Kiyong Kim <scgyong@tukorea.ac.kr>
2024-09-12 01:39:57 +0900

    Space Bar 를 누르면 프레임당 x축 방향으로 1.2, y축 방향으로 0.7 만큼씩 직선 운동을 시킨다.

--- src/w03/move_linear.py
+++ src/w03/move_linear.py
@@ -5,26 +5,44 @@ def char_draw():
   character.clip_composite_draw(100 * frame_index, 0, 100, 100, 0, flip, x, y, 100, 100)
   frame_index = (frame_index + 1) % 8
 
+def reset():
+  global x, y
+  x, y = 100, 100
+
+def update_delta():
+  global x, y
+  x += dx
+  y += dy
+  # print(f'{x=} {y=} {dx=} {dy=}')
+
 def handle_events():
-  global running
+  global running, update
   for e in get_events():
     if e.type == SDL_QUIT:
       running = False
     elif e.type == SDL_KEYDOWN:
       if e.key == SDLK_ESCAPE:
         running = False
+      elif e.key == SDLK_SPACE:
+        global dx, dy
+        dx, dy = 1.2, 0.7
+        reset()
+        update = update_delta
+
 
 open_canvas()
 
 character = load_image('run_animation.png')
 frame_index = 0
 flip = ''
-x, y = 100, 100
+reset()
+update = reset
 
 running = True
 while running:
   clear_canvas()
   char_draw()
+  update()
 
   update_canvas()
   handle_events()
