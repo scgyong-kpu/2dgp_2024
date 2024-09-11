9a0626efcdea1380cbc22169aca1c20fbd31a328
Kiyong Kim <scgyong@tukorea.ac.kr>
2024-09-11 23:21:08 +0900

    flip 을 바꾸어 가면서 호출.

--- src/w03/character_move.py
+++ src/w03/character_move.py
@@ -9,9 +9,12 @@ frame_index = 0
 rad = 0
 flip = ''
 
-for x in range(0, 800, 5):
+for x in range(0, 800):
   clear_canvas()
   grass.draw(400, 30)
+  flip = 'h' if x // 200 % 2 == 0 else ''
+  flip += 'v' if (x - 100) // 200 % 2 == 0 else ''
+  print(f'{x=:3d}, {flip=}')
   character.clip_composite_draw(100 * frame_index, 0, 100, 100, rad, flip, x, 90, 200, 200)
   frame_index = (frame_index + 1) % 8
   update_canvas()
