36692b8e898cda59098626467c67f365ddc02925
Kiyong Kim <scgyong@tukorea.ac.kr>
2024-09-11 23:24:16 +0900

    회전 추가.

--- src/w03/character_move.py
+++ src/w03/character_move.py
@@ -12,9 +12,11 @@ flip = ''
 for x in range(0, 800):
   clear_canvas()
   grass.draw(400, 30)
-  flip = 'h' if x // 200 % 2 == 0 else ''
-  flip += 'v' if (x - 100) // 200 % 2 == 0 else ''
-  print(f'{x=:3d}, {flip=}')
+  # flip = 'h' if x // 200 % 2 == 0 else ''
+  # flip += 'v' if (x - 100) // 200 % 2 == 0 else ''
+  rad += math.pi / 100
+  # pi/100 만큼 더하고 있어서 x 가 100 증가할때마다 반바퀴씩 돈다.
+  print(f'{x=:3d}, {flip=}, {rad=:5.2f}')
   character.clip_composite_draw(100 * frame_index, 0, 100, 100, rad, flip, x, 90, 200, 200)
   frame_index = (frame_index + 1) % 8
   update_canvas()
