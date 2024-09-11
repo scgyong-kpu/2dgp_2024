650a1430a41ec5dd8c760bba2115a8352f331c07
Kiyong Kim <scgyong@tukorea.ac.kr>
2024-09-11 23:06:47 +0900

    0 부터 7 까지 반복하는 동일한 코드.

--- src/w03/character_move.py
+++ src/w03/character_move.py
@@ -10,8 +10,7 @@ for x in range(0, 800, 5):
   clear_canvas()
   grass.draw(400, 30)
   character.clip_draw(100 * frame_index, 0, 100, 100, x, 90)
-  frame_index += 1
-  if frame_index == 8: frame_index = 0
+  frame_index = (frame_index + 1) % 8
   update_canvas()
   delay(0.01)
 
