d324fc2e5b7b5c80543281ef8cea54968b0f883f
asd <sad@ads>
2024-09-24 15:26:40 +0900

    Game Framework: Image cache

--- src/w04/boy.py
+++ src/w04/boy.py
@@ -1,17 +1,16 @@
 from pico2d import *
 import random
+import gfw_image
 
 class Boy:
-    image = None
     def __init__(self):
-        if Boy.image == None:
-            Boy.image = load_image('run_animation.png')
+        self.image = gfw_image.load('run_animation.png')
         self.frameIndex = random.randint(0, 7)
         self.x = random.randint(100, 700)
         self.y = random.randint(90, 500)
     def draw(self):
         x = self.frameIndex * 100
-        Boy.image.clip_draw(x, 0, 100, 100, self.x, self.y)
+        self.image.clip_draw(x, 0, 100, 100, self.x, self.y)
     def update(self):
         self.frameIndex = (self.frameIndex + 1) % 8
 
