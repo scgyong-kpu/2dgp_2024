05bb0f1a0a1a4a39dcfe80ba3ad6a3c5f9ffe28e
Kiyong Kim <scgyong@tukorea.ac.kr>
2024-09-26 00:52:38 +0900

    open_canvas 를 한 후 game loop 에 들어가기 전에 해야 할 일 (객체 생성등) 을 할 함수를 전달한다. 이를 callback 이라 부른다.

--- src/w04/gfw_loop.py
+++ src/w04/gfw_loop.py
@@ -3,9 +3,12 @@ from pico2d import *
 game_objects = []
 running = True
 
-def start():
+def start(enter_function):
     global running
     open_canvas() # canvas 를 열어서 화면을 준비한다
+
+    enter_function() # canvas 를 연 뒤에 해야 할 일이 있다면 하게 해 준다
+
     while running: # 무한루프를 돈다
         # update() 를 수행한다 (Game Logic)
         for go in game_objects:
