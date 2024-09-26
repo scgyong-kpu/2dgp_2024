62a570d460b768b0b5019b42ec5de09d788d4ac0
Kiyong Kim <scgyong@tukorea.ac.kr>
2024-09-26 11:43:15 +0900

    main_scene 이 Boy 를 아는 (know) 것은 당연하므로 import 한다. 하지만 Boy 가 main_scene 을 아는 것은 필요가 없고, 단지 scene 이라는 것만 알면 된다. framework 로부터 알아오자.

--- src/w04/boy.py
+++ src/w04/boy.py
@@ -2,7 +2,7 @@ from pico2d import *
 import random
 import gfw_image
 from ball import Ball
-import main_scene
+import gfw
 
 class Boy:
     def __init__(self):
@@ -24,16 +24,4 @@ class Boy:
                 self.x += 10
             elif e.key == SDLK_SPACE:
                 ball = Ball()
-                main_scene.game_objects.append(ball)
-
-''' 상호참조의 문제가 생긴다: 아래 메시지 중 circular import 에 주목하자.
-Pico2d is prepared.
-Traceback (most recent call last):
-  File "D:/Lectures/2024_2/2dgp/git/src/w04/main_scene.py", line 6, in <module>
-    from boy import Boy
-  File "D:/Lectures/2024_2/2dgp/git/src/w04/boy.py", line 5, in <module>
-    import main_scene
-  File "D:/Lectures/2024_2/2dgp/git/src/w04/main_scene.py", line 6, in <module>
-    from boy import Boy
-ImportError: cannot import name 'Boy' from partially initialized module 'boy' (most likely due to a circular import) (D:/Lectures/2024_2/2dgp/git/src/w04/boy.py)
-'''
+                gfw.stack[-1].game_objects.append(ball)
