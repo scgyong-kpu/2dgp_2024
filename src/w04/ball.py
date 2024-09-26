71228d36fe5c6c351316a872f6b5b8e0c5916887
Kiyong Kim <scgyong@tukorea.ac.kr>
2024-09-26 11:31:57 +0900

    Ball 을 ball.py 로 분리.

---
+++ src/w04/ball.py
@@ -0,0 +1,20 @@
+from pico2d import *
+from random import randrange, uniform
+
+class Ball:
+    def __init__(self):
+        self.image = load_image('ball_41x41.png')
+        self.x = randrange(100, 700)
+        self.y = randrange(100, 500)
+        self.dx = uniform(1.0, 2.0) * (1 if randrange(2) == 0 else -1)
+        self.dy = uniform(1.0, 2.0) * (1 if randrange(2) == 0 else -1)
+    def draw(self):
+        self.image.draw(self.x, self.y)
+    def update(self):
+        self.x += self.dx
+        self.y += self.dy
+        l,t,r,b = 25,35,25,35
+        if (self.dx < 0 and self.x < l) or (self.dx > 0 and self.x > get_canvas_width() - r):
+            self.dx *= -1
+        if (self.dy < 0 and self.y < t) or (self.dy > 0 and self.y > get_canvas_height() - b):
+            self.dy *= -1
--- src/w04/sub_scene.py
+++ src/w04/sub_scene.py
@@ -1,27 +1,5 @@
-from pico2d import * 
 import gfw
-from random import randrange, uniform
-
-# import main_scene
-# main_scene 으로 전환하는 것이 아니고 나만 종료 (pop) 하면 된다.
-
-class Ball:
-    def __init__(self):
-        self.image = load_image('ball_41x41.png')
-        self.x = randrange(100, 700)
-        self.y = randrange(100, 500)
-        self.dx = uniform(1.0, 2.0) * (1 if randrange(2) == 0 else -1)
-        self.dy = uniform(1.0, 2.0) * (1 if randrange(2) == 0 else -1)
-    def draw(self):
-        self.image.draw(self.x, self.y)
-    def update(self):
-        self.x += self.dx
-        self.y += self.dy
-        l,t,r,b = 25,35,25,35
-        if (self.dx < 0 and self.x < l) or (self.dx > 0 and self.x > get_canvas_width() - r):
-            self.dx *= -1
-        if (self.dy < 0 and self.y < t) or (self.dy > 0 and self.y > get_canvas_height() - b):
-            self.dy *= -1
+from ball import Ball
 
 game_objects = []
 
