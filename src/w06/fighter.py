4cbfeb86f7a8279c42e9b5777665decbada54f19
Kiyong Kim <scgyong@tukorea.ac.kr>
2024-09-28 03:40:06 +0900

    SPARK_INTERVAL 이내의 시간이면 Spark image 를 추가로 그린다. 비행기의 좌표와 spark 의 좌표는 조금 다르므로 y 좌표를 조정해 주어야 한다.

--- src/w06/fighter.py
+++ src/w06/fighter.py
@@ -10,6 +10,7 @@ class Fighter(gfw.Sprite):
     }
     LASER_INTERVAL = 0.25
     SPARK_INTERVAL = 0.05
+    SPARK_OFFSET = 28
     def __init__(self):
         super().__init__('res/fighter.png', get_canvas_width() // 2, 80)
         self.dx = 0
@@ -18,6 +19,7 @@ class Fighter(gfw.Sprite):
         self.min_x = half_width
         self.max_x = get_canvas_width() - half_width
         self.laser_time = 0
+        self.spark_image = gfw.image.load('res/laser_0.png')
     def handle_event(self, e):
         pair = (e.type, e.key)
         if pair in Fighter.KEY_MAP:
@@ -28,6 +30,10 @@ class Fighter(gfw.Sprite):
         self.laser_time += gfw.frame_time
         if self.laser_time >= Fighter.LASER_INTERVAL:
             self.fire()
+    def draw(self):
+        super().draw()
+        if self.laser_time < Fighter.SPARK_INTERVAL:
+            self.spark_image.draw(self.x, self.y + Fighter.SPARK_OFFSET)
     def fire(self):
         self.laser_time = 0
         print('fire!')
