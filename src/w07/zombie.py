7e919bc5633b090da5479c86a164c739dc3bda5c
Kiyong Kim <scgyong@tukorea.ac.kr>
2024-10-10 01:50:30 +0900

    이미지 여러 장 로드하는 함수 추가.

--- src/w07/zombie.py
+++ src/w07/zombie.py
@@ -1,5 +1,6 @@
 import random
 import time
+import os
 from pico2d import *
 from gfw import *
 
@@ -9,10 +10,21 @@ class Zombie(AnimSprite):
     def __init__(self):
         x, y = random.randrange(get_canvas_width()), random.randrange(get_canvas_height())
         super().__init__('res/zombiefiles/male/Idle (1).png', x, y, self.FPS, 1)
-        self.frame_count = 15
+        self.images = load_image_series('res/zombiefiles/male/Idle (%d).png')
+        self.frame_count = len(self.images)
     def draw(self):
         elpased = time.time() - self.created_on
         index = round(elpased * self.fps) % self.frame_count
-        file = 'res/zombiefiles/male/Idle (%d).png' % (index + 1)
-        image = gfw.image.load(file)
+        image = self.images[index]
         image.draw(self.x, self.y, self.width // self.SCALE, self.height // self.SCALE)
+
+def load_image_series(fmt):
+    images = []
+    index = 1
+    while True:
+        fn = fmt % index
+        if not os.path.isfile(fn):
+            break
+        images.append(gfw.image.load(fn))
+        index += 1
+    return images
