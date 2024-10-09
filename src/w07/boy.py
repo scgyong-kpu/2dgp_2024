6b1e55475fb65315be044436131c7ebedf21660f
Kiyong Kim <scgyong@tukorea.ac.kr>
2024-10-09 20:53:06 +0900

    scrollTo() 대신 scroll() 로 호출할 수도 있다.

--- src/w07/boy.py
+++ src/w07/boy.py
@@ -21,9 +21,9 @@ class Boy:
         self.time += gfw.frame_time
         fps, frame_count = 10, 8
         self.frame = round(self.time * fps) % frame_count
-        scroll_x = self.bg.x + self.dx * self.speed * gfw.frame_time
-        scroll_y = self.bg.y + self.dy * self.speed * gfw.frame_time
-        self.bg.scrollTo(scroll_x, scroll_y)
+        dx = self.dx * self.speed * gfw.frame_time
+        dy = self.dy * self.speed * gfw.frame_time
+        self.bg.scroll(dx, dy)
 
     def handle_event(self, e):
         dx, dy = self.dx, self.dy
