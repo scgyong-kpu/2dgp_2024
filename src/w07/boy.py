db6ed39c28a4d941e97cfb787325b94008c2ea5c
Kiyong Kim <scgyong@tukorea.ac.kr>
2024-10-10 01:14:27 +0900

    Mouse 버튼이 눌리면 해당 위치를 target 으로 설정하며 target 에 도착할때 까지는 마우스가 움직이면 계속 target 을 변경한다. 키보드가 눌리면 target 을 없애(None)고, dx 와 dy 가 다시 -1, 0, 1 의 값을 가지게 한다.

--- src/w07/boy.py
+++ src/w07/boy.py
@@ -12,6 +12,7 @@ class Boy:
         self.speed = 200
         self.action = 3 # 3=StandRight, 2=StandLeft, 1=RunRight, 0=RunLeft
         self.mag = 1
+        self.target = None
 
     def draw(self):
         x = self.frame * 100
@@ -25,35 +26,63 @@ class Boy:
         self.frame = round(self.time * fps) % frame_count
         self.x += self.dx * self.speed * self.mag * gfw.frame_time
         self.y += self.dy * self.speed * self.mag * gfw.frame_time
+        if self.target is not None:
+            tx, ty = self.target
+            if (self.dx > 0 and self.x >= tx) or (self.dx < 0 and self.x <= tx):
+                self.x, self.dx = tx, 0
+            if (self.dy > 0 and self.y >= ty) or (self.dy < 0 and self.y <= ty):
+                self.y, self.dy = ty, 0
+            if self.dx == 0 and self.dy == 0:
+                self.target = None
+                self.adjust_action()
         self.bg.show(self.x, self.y)
+
+    def adjust_delta(self, x, y):
+        if self.target is not None:
+            self.dx, self.dy = 0, 0
+            self.target = None
+        self.dx += x
+        self.dy += y
+
+    def set_target(self, mx, my):
+        tx, ty = self.bg.from_screen(mx, my)
+        if self.x == tx and self.y == ty:
+            self.target = None
+            self.dx, self.dy = 0
+            return
+        self.target = tx, ty
+        rad = math.atan2(ty - self.y, tx - self.x)
+        self.dx, self.dy = math.cos(rad), math.sin(rad)
 
     def handle_event(self, e):
         dx, dy = self.dx, self.dy
         if e.type == SDL_KEYDOWN:
-            if e.key == SDLK_LEFT:
-                self.dx -= 1
-            elif e.key == SDLK_RIGHT:
-                self.dx += 1
-            elif e.key == SDLK_DOWN:
-                self.dy -= 1
-            elif e.key == SDLK_UP:
-                self.dy += 1
+            if e.key == SDLK_LEFT:    self.adjust_delta(-1, 0)
+            elif e.key == SDLK_RIGHT: self.adjust_delta(1, 0)
+            elif e.key == SDLK_DOWN:  self.adjust_delta(0, -1)
+            elif e.key == SDLK_UP:    self.adjust_delta(0, 1)
             elif e.key == SDLK_LSHIFT:
                 self.mag *= 2
 
         elif e.type == SDL_KEYUP:
-            if e.key == SDLK_LEFT:
-                self.dx += 1
-            elif e.key == SDLK_RIGHT:
-                self.dx -= 1
-            elif e.key == SDLK_DOWN:
-                self.dy += 1
-            elif e.key == SDLK_UP:
-                self.dy -= 1
+            if e.key == SDLK_LEFT:    self.adjust_delta(1, 0)
+            elif e.key == SDLK_RIGHT: self.adjust_delta(-1, 0)
+            elif e.key == SDLK_DOWN:  self.adjust_delta(0, 1)
+            elif e.key == SDLK_UP:    self.adjust_delta(0, -1)
             elif e.key == SDLK_LSHIFT:
                 self.mag //= 2
 
+        elif e.type == SDL_MOUSEBUTTONDOWN:
+            self.set_target(e.x, get_canvas_height() - e.y - 1)
+        elif e.type == SDL_MOUSEMOTION:
+            if self.target is not None:
+                self.set_target(e.x, get_canvas_height() - e.y - 1)
+
+        # print(f'({dx=}, {dy=}) != ({self.dx=}, {self.dy=})')
         if (dx, dy) != (self.dx, self.dy):
+            self.adjust_action()
+
+    def adjust_action(self):
             if self.dx > 0:
                 self.action = 1
             elif self.dx < 0:
