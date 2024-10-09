460b641bb37b9f1712aa566e11ff20ee20e14c30
Kiyong Kim <scgyong@tukorea.ac.kr>
2024-10-10 01:42:08 +0900

    크기를 1/4 로 그리고 15장의 이미지로 초당 12 프레임 애니메이션 진행

--- src/w07/zombie.py
+++ src/w07/zombie.py
@@ -1,9 +1,18 @@
 import random
+import time
 from pico2d import *
 from gfw import *
 
 class Zombie(AnimSprite):
-    FPS = 8
+    FPS = 12
+    SCALE = 4
     def __init__(self):
         x, y = random.randrange(get_canvas_width()), random.randrange(get_canvas_height())
         super().__init__('res/zombiefiles/male/Idle (1).png', x, y, self.FPS, 1)
+        self.frame_count = 15
+    def draw(self):
+        elpased = time.time() - self.created_on
+        index = round(elpased * self.fps) % self.frame_count
+        file = 'res/zombiefiles/male/Idle (%d).png' % (index + 1)
+        image = gfw.image.load(file)
+        image.draw(self.x, self.y, self.width // self.SCALE, self.height // self.SCALE)
