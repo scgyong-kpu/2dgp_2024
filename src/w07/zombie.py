dfc7f4b855cd473662460d2c2ea0dbd5c74fe10d
Kiyong Kim <scgyong@tukorea.ac.kr>
2024-10-10 20:09:07 +0900

    State 별로 class 를 만들지 않고 Zombie 내의 member 함수들로 분기하게끔 한다.

--- src/w07/zombie.py
+++ src/w07/zombie.py
@@ -4,58 +4,37 @@ import os
 from pico2d import *
 from gfw import *
 
-class Idle:
-    def __init__(self):
-        self.name = 'Idle'
-    def update(self, zombie):
-        pass
-    def draw(self, zombie):
-        pass
-class Walk:
-    def __init__(self):
-        self.name = 'Walk'
-    def update(self, zombie):
-        pass
-    def draw(self, zombie):
-        pass
-class Attack:
-    def __init__(self):
-        self.name = 'Attack'
-    def update(self, zombie):
-        zombie.x += random.uniform(-30, 30) * gfw.frame_time
-        zombie.y += random.uniform(-30, 30) * gfw.frame_time
-        # return False
-    def draw(self, zombie):
-        pass
-class Dead:
-    def __init__(self):
-        self.name = 'Dead'
-    def enter(self, zombie):
-        zombie.time = 0
-    def update(self, zombie):
-        zombie.time += gfw.frame_time
-        if zombie.time > 2:
-            world = gfw.top().world
-            world.remove(zombie, world.layer.zombie)
-        return True
-    def draw(self, zombie):
-        main_scene = gfw.top()
-        screen_pos = main_scene.bg.to_screen(zombie.x, zombie.y)
-        index = min(round(zombie.time * zombie.fps), len(zombie.images) - 1)
-        image = zombie.images[index]
-        image.composite_draw(0, zombie.flip, *screen_pos, zombie.width, zombie.height)
-        return True
+
+class SubState:
+    def __init__(self, **funcs):
+        self.funcs = funcs
+    def draw(self):
+        self.funcs['draw']()
+    def update(self):
+        self.funcs['update']()
+    def enter(self):
+        if 'enter' in self.funcs:
+            self.funcs['enter']()
+    def exit(self):
+        if 'exit' in self.funcs:
+            self.funcs['exit']()
 
 class Zombie(AnimSprite):
     FPS = 12
     SCALE = 4
     GENDERS = ['male', 'female']
-    STATES = [Idle(), Walk(), Attack(), Dead()]
+    NEXTS = {'Idle':'Walk', 'Walk':'Attack', 'Attack':'Dead'}
     def __init__(self):
         x, y = random.randrange(get_canvas_width()), random.randrange(get_canvas_height())
         super().__init__(None, x, y, self.FPS, 1)
         self.gender = random.choice(self.GENDERS)
-        self.set_state(0) # loads images
+        self.states = {
+            'Idle': SubState(draw=self.draw_normal, update=self.update_normal),
+            'Walk': SubState(draw=self.draw_normal, update=self.update_normal),
+            'Attack': SubState(draw=self.draw_normal, update=self.update_attack),
+            'Dead': SubState(draw=self.draw_dead, update=self.update_dead, enter=self.enter_dead),
+        }
+        self.set_state('Idle')
         self.width = self.images[0].w // self.SCALE
         self.height = self.images[0].h // self.SCALE
         self.flip = random.choice(['', 'h'])
@@ -63,16 +42,14 @@ class Zombie(AnimSprite):
     def load_images(self):
         self.images = load_image_series(f'res/zombiefiles/{self.gender}/{self.action} (%d).png')
         self.frame_count = len(self.images)
-    def set_state(self, state_index):
-        self.state_index = state_index
-        self.state = self.STATES[state_index]
-        self.action = self.state.name
+    def set_state(self, state_name):
+        self.state = self.states[state_name]
+        self.action = state_name
         self.load_images()
-        if hasattr(self.state, 'enter'):
-            self.state.enter(self)
+        self.state.enter()
     def draw(self):
-        if self.state.draw(self):
-            return
+        self.state.draw()
+    def draw_normal(self):
         main_scene = gfw.top()
         screen_pos = main_scene.bg.to_screen(self.x, self.y)
         elpased = time.time() - self.created_on
@@ -80,12 +57,30 @@ class Zombie(AnimSprite):
         image = self.images[index]
         image.composite_draw(0, self.flip, *screen_pos, self.width, self.height)
     def update(self):
-        if self.state.update(self):
-            return
+        self.state.update()
+    def update_normal(self):
         self.time -= gfw.frame_time
         if self.time <= 0:
             self.time = random.uniform(2, 10)
-            self.set_state((self.state_index + 1) % len(self.STATES))
+            next_state = self.NEXTS[self.action]
+            self.set_state(next_state)
+    def update_attack(self):
+        self.x += random.uniform(-30, 30) * gfw.frame_time
+        self.y += random.uniform(-30, 30) * gfw.frame_time
+        self.update_normal()
+    def enter_dead(self):
+        self.time = 0
+    def update_dead(self):
+        self.time += gfw.frame_time
+        if self.time > 2:
+            world = gfw.top().world
+            world.remove(self, world.layer.zombie)
+    def draw_dead(self):
+        main_scene = gfw.top()
+        screen_pos = main_scene.bg.to_screen(self.x, self.y)
+        index = min(round(self.time * self.fps), len(self.images) - 1)
+        image = self.images[index]
+        image.composite_draw(0, self.flip, *screen_pos, self.width, self.height)
 
     def get_bb(self):
         half_width  = self.width * 9 // 20
