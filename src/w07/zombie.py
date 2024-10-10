45452c8ef858cc694e6242cfe3385e200477d2ce
Kiyong Kim <scgyong@tukorea.ac.kr>
2024-10-10 18:30:15 +0900

    Action 별 이미지가 다른 경우, 액션이 변경될때마다 이미지셋을 다시 로드한다.

--- src/w07/zombie.py
+++ src/w07/zombie.py
@@ -7,17 +7,24 @@ from gfw import *
 class Zombie(AnimSprite):
     FPS = 12
     SCALE = 4
+    GENDERS = ['male', 'female']
     ACTIONS = ['Attack', 'Dead', 'Idle', 'Walk']
     def __init__(self):
         x, y = random.randrange(get_canvas_width()), random.randrange(get_canvas_height())
         super().__init__(None, x, y, self.FPS, 1)
-        gender = random.choice(['male', 'female'])
-        action = random.choice(self.ACTIONS)
-        self.images = load_image_series(f'res/zombiefiles/{gender}/{action} (%d).png')
+        self.gender = random.choice(self.GENDERS)
+        self.set_action('Idle') # loads images
         self.width = self.images[0].w // self.SCALE
         self.height = self.images[0].h // self.SCALE
-        self.frame_count = len(self.images)
         self.flip = random.choice(['', 'h'])
+        self.time = 1
+        self.action_index = 2
+    def load_images(self):
+        self.images = load_image_series(f'res/zombiefiles/{self.gender}/{self.action} (%d).png')
+        self.frame_count = len(self.images)
+    def set_action(self, action):
+        self.action = action
+        self.load_images()
     def draw(self):
         main_scene = gfw.top()
         screen_pos = main_scene.bg.to_screen(self.x, self.y)
@@ -25,6 +32,13 @@ class Zombie(AnimSprite):
         index = round(elpased * self.fps) % self.frame_count
         image = self.images[index]
         image.composite_draw(0, self.flip, *screen_pos, self.width, self.height)
+    def update(self):
+        self.time -= gfw.frame_time
+        if self.time <= 0:
+            self.time = random.uniform(2, 10)
+            self.action_index = (self.action_index + 1) % len(self.ACTIONS)
+            self.set_action(self.ACTIONS[self.action_index])
+
     def get_bb(self):
         half_width  = self.width * 9 // 20
         half_height = self.height * 9 // 20
