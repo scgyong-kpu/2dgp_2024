788c75898d3afad9ab44ed54911ad1ce20035d1c
Kiyong Kim <scgyong@tukorea.ac.kr>
2024-10-10 21:25:17 +0900

    Idle이 아니면 Walk 이도록 Sequence 만 가지고 만들어 본다. 하지만 움직이는 동안 근처에 소년이 있는지 확인하고, 근처에 다가오면 쫒아가는 등 복잡한 동작을 만들기는 어렵다.

--- src/w07/zombie.py
+++ src/w07/zombie.py
@@ -4,83 +4,105 @@ import os
 from pico2d import *
 from gfw import *
 
+class Node:
+    def add_child(self, child):
+        self.children.append(child)
+    def add_children(self, *children):
+        for child in children:
+            self.children.append(child)
 
-class SubState:
-    def __init__(self, **funcs):
-        self.funcs = funcs
-    def draw(self):
-        self.funcs['draw']()
-    def update(self):
-        self.funcs['update']()
-    def enter(self):
-        if 'enter' in self.funcs:
-            self.funcs['enter']()
-    def exit(self):
-        if 'exit' in self.funcs:
-            self.funcs['exit']()
+class SelectorNode(Node):
+    def __init__(self, name):
+        self.children = []
+        self.name = name
+        self.prev_running_pos = 0
+    def run(self):
+        pass
+
+class SequenceNode(Node):
+    def __init__(self, name):
+        self.children = []
+        self.name = name
+    def run(self):
+        for pos in range(len(self.children)):
+            result = self.children[pos].run()
+            if result == BehaviorTree.SUCCESS:
+                return BehaviorTree.SUCCESS
+        return BehaviorTree.FAIL
+
+class LeafNode(Node):
+    def __init__(self, name, func):
+        self.name = name
+        self.func = func
+    def run(self):
+        return self.func()
+
+class BehaviorTree:
+    FAIL, RUNNING, SUCCESS = -1, 0, 1
+
+    def __init__(self, root_node):
+        self.root = root_node
+
+    def run(self):
+        self.root.run()
+
 
 class Zombie(AnimSprite):
     FPS = 12
     SCALE = 4
     GENDERS = ['male', 'female']
-    NEXTS = {'Idle':'Walk', 'Walk':'Attack', 'Attack':'Dead'}
+    IDLE_INTERVAL = 2.0
+    DEAD_INTERVAL = 2.0
     def __init__(self):
         x, y = random.randrange(get_canvas_width()), random.randrange(get_canvas_height())
         super().__init__(None, x, y, self.FPS, 1)
         self.gender = random.choice(self.GENDERS)
-        self.states = {
-            'Idle': SubState(draw=self.draw_normal, update=self.update_normal),
-            'Walk': SubState(draw=self.draw_normal, update=self.update_normal),
-            'Attack': SubState(draw=self.draw_normal, update=self.update_attack),
-            'Dead': SubState(draw=self.draw_dead, update=self.update_dead, enter=self.enter_dead),
-        }
-        self.set_state('Idle')
+        self.build_behavior_tree()
+        self.set_action('Idle')
+        self.load_images()
         self.width = self.images[0].w // self.SCALE
         self.height = self.images[0].h // self.SCALE
         self.flip = random.choice(['', 'h'])
-        self.time = 1
+        self.speed = random.uniform(100, 150) * (1 if self.flip == '' else -1)
+    def set_action(self, action):
+        self.action = action
+        self.load_images()
+        self.time = 0
     def load_images(self):
         self.images = load_image_series(f'res/zombiefiles/{self.gender}/{self.action} (%d).png')
         self.frame_count = len(self.images)
-    def set_state(self, state_name):
-        self.state = self.states[state_name]
-        self.action = state_name
-        self.load_images()
-        self.state.enter()
     def draw(self):
-        self.state.draw()
-    def draw_normal(self):
         main_scene = gfw.top()
         screen_pos = main_scene.bg.to_screen(self.x, self.y)
-        elpased = time.time() - self.created_on
-        index = round(elpased * self.fps) % self.frame_count
+        index = round(self.time * self.fps)
+        if self.action != 'Dead':
+            index %= self.frame_count
+        else:
+            index = min(index, self.frame_count)
         image = self.images[index]
         image.composite_draw(0, self.flip, *screen_pos, self.width, self.height)
     def update(self):
-        self.state.update()
-    def update_normal(self):
-        self.time -= gfw.frame_time
-        if self.time <= 0:
-            self.time = random.uniform(2, 10)
-            next_state = self.NEXTS[self.action]
-            self.set_state(next_state)
-    def update_attack(self):
-        self.x += random.uniform(-30, 30) * gfw.frame_time
-        self.y += random.uniform(-30, 30) * gfw.frame_time
-        self.update_normal()
-    def enter_dead(self):
-        self.time = 0
-    def update_dead(self):
         self.time += gfw.frame_time
-        if self.time > 2:
+        self.bt.run()
+
+    def do_idle(self):
+        if self.action != 'Idle': 
+            return BehaviorTree.FAIL
+        if self.time < self.IDLE_INTERVAL:
+            return BehaviorTree.SUCCESS
+        self.set_action('Walk')
+        return BehaviorTree.FAIL
+
+    def do_walk(self):
+        self.x += self.speed * gfw.frame_time
+        if (self.speed < 0 and self.x < 0) or (self.speed > 0 and self.x > get_canvas_width()):
+            self.speed *= -1
+            self.flip = '' if self.speed > 0 else 'h'
+        return BehaviorTree.SUCCESS
+    def do_dead(self):
+        if self.time > self.DEAD_INTERVAL:
             world = gfw.top().world
             world.remove(self, world.layer.zombie)
-    def draw_dead(self):
-        main_scene = gfw.top()
-        screen_pos = main_scene.bg.to_screen(self.x, self.y)
-        index = min(round(self.time * self.fps), len(self.images) - 1)
-        image = self.images[index]
-        image.composite_draw(0, self.flip, *screen_pos, self.width, self.height)
 
     def get_bb(self):
         half_width  = self.width * 9 // 20
@@ -90,6 +112,12 @@ class Zombie(AnimSprite):
         r = self.x + half_width
         t = self.y + half_width
         return l, b, r, t
+
+    def build_behavior_tree(self):
+        seq = SequenceNode('Sequence')
+        seq.add_child(LeafNode('Idle', self.do_idle))
+        seq.add_child(LeafNode('Walk', self.do_walk))
+        self.bt = BehaviorTree(seq)
 
 def load_image_series(fmt):
     images = []
