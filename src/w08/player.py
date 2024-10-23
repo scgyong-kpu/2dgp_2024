87799b3740c79a7143d3d7779240daacfe7dc5b9
Kiyong Kim <scgyong@tukorea.ac.kr>
2024-10-24 00:29:57 +0900

    src_rect 를 변경하면서 그려 애니메이션이 되게 한다.

--- src/w08/player.py
+++ src/w08/player.py
@@ -1,22 +1,33 @@
 from pico2d import * 
 from gfw import *
+import time
 
-class Cookie(Sprite):
+class Cookie(AnimSprite):
     def __init__(self):
-        super().__init__('res/cookie.png', 160, 160)
+        super().__init__('res/cookie.png', 160, 160, 10, 1)
         self.running = True
         self.width, self.height = 138, 138
-        self.src_rect = 69, 1090, 138, 138
+        self.src_rects = [ 
+            ( 69, 1090, 138, 138), 
+            (341, 1090, 138, 138), 
+            (613, 1090, 138, 138), 
+            (885, 1090, 138, 138), 
+        ]
 
     def handle_event(self, e):
         if e.type == SDL_KEYDOWN and e.key == SDLK_SPACE:
             self.toggle_state()
 
     def draw(self):
-        self.image.clip_draw(*self.src_rect, self.x, self.y)
+        elpased = time.time() - self.created_on
+        frame_count = len(self.src_rects)
+        index = round(elpased * self.fps) % frame_count
+        src_rect = self.src_rects[index]
+        self.image.clip_draw(*src_rect, self.x, self.y)
 
     def toggle_state(self):
         self.running = not self.running
         fn = 'res/cookie_run.png' if self.running else 'res/cookie_jump.png'
         self.image = load_image(fn)
         self.frame_count = self.image.w // self.image.h
+
