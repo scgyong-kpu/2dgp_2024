375797ef64140d6b02ddb557cede30f1ecb5046b
Kiyong Kim <scgyong@tukorea.ac.kr>
2024-10-24 08:48:36 +0900

    맵 전체를 init 에서 한 번에 생성하지 않고 필요할때 update()  에서 생성한다.

--- src/w08/floor.py
+++ src/w08/floor.py
@@ -48,13 +48,12 @@ class JellyItem(MapObject):
     def draw(self):
         self.image.clip_draw(*self.src_rect, self.x, self.y)
 
-def mapobject_factory_create(tile, map_x, map_y):
-    left, top = map_x * UNIT, (9-map_y-1) * UNIT
+def mapobject_factory_create(tile, left, bottom):
     if tile >= 1 and tile <= 60:
-        return JellyItem(tile - 1, left, top)
+        return JellyItem(tile - 1, left, bottom)
     if tile == 91 or tile == 101 or tile == 73:
         type = Floor.TYPE_10x2 if tile == 91 else Floor.TYPE_2x2 if tile == 101 else Floor.TYPE_3x1
-        return Floor(type, left, top)
+        return Floor(type, left, bottom)
     return None
 
 def init():
@@ -62,41 +61,32 @@ def init():
         tmj = json.load(f)
         # print(tmj)
 
+    global map_width, map_height, map_data
+    global last_floor_right, map_x
     world = gfw.top().world
     map_width = tmj['width']
     map_height = tmj['height']
-    data = tmj['layers'][0]['data']
-    index = 0
-    for y in range(map_height):
-        for x in range(map_width):
-            tile = data[index]
-            obj = mapobject_factory_create(tile, x, y)
-            if obj is not None:
-                world.append(obj)
-            index += 1
-    # print(tmj['width'], data)
-    # global last_floor_right
-    # last_floor_right = 0
+    map_data = tmj['layers'][0]['data']
+    last_floor_right = 0
+    map_x = 0
 def draw():
     pass
 def update():
-    pass
-    # global last_floor_right
-    # last_floor_right += SPEED * gfw.frame_time
-    # # print(f'{last_floor_right=:.1f} {17*UNIT}')
-    # while last_floor_right < 17 * UNIT: # screen = 16x9 UNIT
-    #     t = Floor.TYPE_20x2 if random.random() < 0.5 else Floor.TYPE_2x2
-    #     f = Floor(t, last_floor_right, 0)
-    #     # print(f'Appending {f} at f.left={f.x-f.width//2:.1f} {f.x=} {f.y=}')
-    #     world = gfw.top().world
-    #     world.append(f)
+    global last_floor_right, map_x
 
-    #     jelly = JellyItem(random.randrange(60), last_floor_right, random.randint(3,8)*UNIT)
-    #     # print(f'Appending {jelly} at jelly.left={jelly.x-jelly.width//2:.1f} {jelly.x=:.1f} {jelly.y=}')
-    #     world.append(jelly)
+    last_floor_right += SPEED * gfw.frame_time
 
-    #     last_floor_right += f.width
-    # if random.random() < 0.01:
-    #     world = gfw.top().world
-    #     world.append(Floor(Floor.TYPE_3x1, 15, random.randint(3, 8)), world.layer.floor)
+    while last_floor_right < 17 * UNIT:
+        bottom = 0
+        for y in range(map_height - 1, -1, -1):
+            tile = map_data[y * map_width + map_x]
+            obj = mapobject_factory_create(tile, last_floor_right, bottom)
+            if obj is not None:
+                # print(f'@({map_x}, {y}) {obj} ({obj.x},{obj.y}) [{obj.x - obj.width//2} ~ {obj.x + obj.width//2}]')
+                world = gfw.top().world
+                world.append(obj)
+            bottom += UNIT
+        map_x += 1
+        last_floor_right += UNIT
+        print(f'{map_x=} / {map_width=}')
 
