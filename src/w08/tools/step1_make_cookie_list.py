0113486f25d8e99801980ff9b9e9dac695e06650
Kiyong Kim <scgyong@tukorea.ac.kr>
2024-11-01 01:41:58 +0900

    Step1-2 - Regular Expression 을 이용한 패턴매칭

--- src/w08/tools/step1_make_cookie_list.py
+++ src/w08/tools/step1_make_cookie_list.py
@@ -1,10 +1,16 @@
-line = 0
+import re
+
+pat = re.compile(r'\s+<img src="/resources/sheet_icons/(\d+)/(\d+).png[^"]*" alt="([^"]+)"[^>]*>\s*$')
+
 with open('out/cookierun.html', 'r') as f:
     while True:
         str = f.readline()
         if not str: break
 
-        line += 1
+        m = pat.match(str)
+        if not m: continue
+        grade, number, name = m.groups()
+        char = { "id": number, "name": name, "grade": grade }
+        print(char)
 
-print(f'{line} lines read.')
 
