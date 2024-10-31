7de67bd1caad865cc2bdb8ac8d99d96d772d5b6f
Kiyong Kim <scgyong@tukorea.ac.kr>
2024-11-01 02:40:10 +0900

    border 2pixel 을 제외했을 때 정사각형이 몇 개 들어 있나 확인해 본다. 1.6 인게 제일 많고, 1.333, 2.5, 1.833, 2.166 순서로 많았다. 몇 가지만 지원하기로 결정해 본다.

--- src/w08/tools/step3_analyze_images.py
+++ src/w08/tools/step3_analyze_images.py
@@ -6,15 +6,43 @@ with open('out/cookies.json', 'r') as f:
 
 pico2d.open_canvas()
 
+ratios = dict()
+
 for cookie in cookies:
     cid = cookie['id']
     fname = f'out/{cid}_sheet.png'
     try:
         image = pico2d.load_image(fname)
-        print(f'{fname=}, w={image.w}, h={image.h}')
+        ratio = (image.w - 2) / (image.h - 2)
+        if ratio in ratios:
+            ratios[ratio] += 1
+        else:
+            ratios[ratio] = 1
+        print(f'{fname=}, w={image.w}, h={image.h} {ratio=:.3f} count={ratios[ratio]}')
         del image
     except:
         pass # ignore error files
 
 pico2d.close_canvas()
 
+counts = list(ratios.items())
+counts.sort(key=lambda item: -item[1])
+
+print(counts)
+
+'''
+
+Pico2d is prepared.
+fname='out/107566_sheet.png', w=2994, h=1634 ratio=1.833 count=1
+fname='out/107567_sheet.png', w=3192, h=1742 ratio=1.833 count=2
+fname='out/107571_sheet.png', w=3544, h=1934 ratio=1.833 count=3
+fname='out/107570_sheet.png', w=3544, h=1934 ratio=1.833 count=4
+fname='out/107569_sheet.png', w=5192, h=2078 ratio=2.500 count=1
+...
+
+[(1.6, 48), (1.3333333333333333, 17), (2.5, 12), (1.8333333333333333, 10), (2.1666666666666665, 7),
+ (3.0, 4), (2.142857142857143, 4), (1.1428571428571428, 4), (2.0, 3), (1.875, 3), 
+ (2.3333333333333335, 2), (2.4, 2), (3.4, 2), (0.7152317880794702, 2),
+...
+
+'''
