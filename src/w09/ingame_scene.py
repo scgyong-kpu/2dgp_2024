58831d522ceaa7f7627e4a5fd419f78d2f645c12
Kiyong Kim <scgyong@tukorea.ac.kr>
2024-11-03 21:06:08 +0900

    Card 클래스 추가하고 1장짜리 애니메이션이 되게 함.

--- src/w09/ingame_scene.py
+++ src/w09/ingame_scene.py
@@ -1,7 +1,7 @@
 from pico2d import * 
 from gfw import *
 
-world = World()
+world = World(['bg', 'card'])
 
 canvas_width = 960
 canvas_height = 540
@@ -11,9 +11,18 @@ shows_object_count = True
 center_x = canvas_width // 2
 center_y = canvas_height // 2
 
+def card_position(x, y):
+    return (x + 1) * 120 - 30, (y + 1) * 120 - 28
+
+class Card(AnimSprite):
+    def __init__(self, x, y):
+        super().__init__('res/back.png', *card_position(x, y), 10)
+
 def enter():
-    world.append(Background('res/bg_andromeda.png'), 0)
-    pass
+    world.append(Background('res/bg_andromeda.png'), world.layer.bg)
+    for y in range(4):
+        for x in range(5):
+            world.append(Card(x, y), world.layer.card)
 
 def exit():
     world.clear()
