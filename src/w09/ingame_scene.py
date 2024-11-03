73f1de8fe27f71d2ae79651dedfa8f59e8a52c3b
Kiyong Kim <scgyong@tukorea.ac.kr>
2024-11-03 21:10:04 +0900

    0~19 번째의 카드가 1~10 까지의 인덱스를 가지게 하고 BoundingBox 는 그리지 않음. (충돌체크할 일이 별로 없다)

--- src/w09/ingame_scene.py
+++ src/w09/ingame_scene.py
@@ -5,7 +5,7 @@ world = World(['bg', 'card'])
 
 canvas_width = 960
 canvas_height = 540
-shows_bounding_box = True
+shows_bounding_box = False #True
 shows_object_count = True
 
 center_x = canvas_width // 2
@@ -15,14 +15,17 @@ def card_position(x, y):
     return (x + 1) * 120 - 30, (y + 1) * 120 - 28
 
 class Card(AnimSprite):
-    def __init__(self, x, y):
-        super().__init__('res/back.png', *card_position(x, y), 10)
+    def __init__(self, x, y, index):
+        super().__init__(f'res/f_{index:02d}.png', *card_position(x, y), 10)
+        self.index = index
 
 def enter():
     world.append(Background('res/bg_andromeda.png'), world.layer.bg)
+    index = 0
     for y in range(4):
         for x in range(5):
-            world.append(Card(x, y), world.layer.card)
+            world.append(Card(x, y, index // 2 + 1), world.layer.card)
+            index += 1
 
 def exit():
     world.clear()
