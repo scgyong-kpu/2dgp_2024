bf19a082e366f90d41370313c7038751ddc86fb9
Kiyong Kim <scgyong@tukorea.ac.kr>
2024-11-03 23:06:44 +0900

    GFW: NinePatch 를 gfw.image 로 옮김.

--- src/w09/gfw/image.py
+++ src/w09/gfw/image.py
@@ -16,3 +16,27 @@ def unload(file):
     if file in _images:
         del _images[file]
 
+class NinePatch:
+    def __init__(self, image, l, b, r, t):
+        self.image = image
+        self.margins = l, b, r, t
+
+    def draw(self, x, y, width, height):
+        self.draw_to_origin(x - width // 2, y - height // 2, width, height)
+
+    def draw_to_origin(self, left, bottom, width, height):
+        l, b, r, t = self.margins
+        iw, ih = self.image.w, self.image.h
+        dr, dt = left + width - r, bottom + height - t
+        scw, sch = iw - l - r, ih - l - r
+        dcw, dch = width - l - r, height - l - r
+        self.image.clip_draw_to_origin(0, 0, l, b, left, bottom) # left-bottom
+        self.image.clip_draw_to_origin(iw - r, 0, r, b, dr, bottom) # right-bottom
+        self.image.clip_draw_to_origin(0, ih - t, l, t, left, dt) # left-top
+        self.image.clip_draw_to_origin(iw - r, ih - t, r, t, dr, dt) # right-top
+        self.image.clip_draw_to_origin(l, 0, scw, b, left + l, bottom, dcw, b) # center-bottom
+        self.image.clip_draw_to_origin(0, b, l, sch, left, bottom + b, l, dch) # left-center
+        self.image.clip_draw_to_origin(iw - r, b, r, sch, dr, bottom + b, r, dch) # right-center
+        self.image.clip_draw_to_origin(l, ih-t, scw, t, left + l, dt, dcw, t) # center-top
+        self.image.clip_draw_to_origin(l, b, scw, sch, left + l, bottom + b, dcw, dch) # center-center
+
--- src/w09/ingame_scene.py
+++ src/w09/ingame_scene.py
@@ -41,35 +41,11 @@ class MainUi:
     def __init__(self):
         self.bg = gfw.image.load(f'res/round_rect_9.png')
         self.font = gfw.font.load(f'res/ENCR10B.TTF')
-        self.nine = NinePatch(self.bg, 24, 24, 24, 24)
+        self.nine = gfw.image.NinePatch(self.bg, 24, 24, 24, 24)
     def update(self): pass
     def draw(self):
         self.nine.draw(790, center_y, 280, 460)
         self.font.draw(680, 450, ('Score: %5.1f' % 123.4))
-
-class NinePatch:
-    def __init__(self, image, l, b, r, t):
-        self.image = image
-        self.margins = l, b, r, t
-
-    def draw(self, x, y, width, height):
-        self.draw_to_origin(x - width // 2, y - height // 2, width, height)
-
-    def draw_to_origin(self, left, bottom, width, height):
-        l, b, r, t = self.margins
-        iw, ih = self.image.w, self.image.h
-        dr, dt = left + width - r, bottom + height - t
-        scw, sch = iw - l - r, ih - l - r
-        dcw, dch = width - l - r, height - l - r
-        self.image.clip_draw_to_origin(0, 0, l, b, left, bottom) # left-bottom
-        self.image.clip_draw_to_origin(iw - r, 0, r, b, dr, bottom) # right-bottom
-        self.image.clip_draw_to_origin(0, ih - t, l, t, left, dt) # left-top
-        self.image.clip_draw_to_origin(iw - r, ih - t, r, t, dr, dt) # right-top
-        self.image.clip_draw_to_origin(l, 0, scw, b, left + l, bottom, dcw, b) # center-bottom
-        self.image.clip_draw_to_origin(0, b, l, sch, left, bottom + b, l, dch) # left-center
-        self.image.clip_draw_to_origin(iw - r, b, r, sch, dr, bottom + b, r, dch) # right-center
-        self.image.clip_draw_to_origin(l, ih-t, scw, t, left + l, dt, dcw, t) # center-top
-        self.image.clip_draw_to_origin(l, b, scw, sch, left + l, bottom + b, dcw, dch) # center-center
 
 
 def enter():
