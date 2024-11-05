f8f2e63e9520a1dcdabe8d7c312494b3c2345133
Kiyong Kim <scgyong@tukorea.ac.kr>
2024-11-05 23:16:54 +0900

    Button class 를 만들어 각각이 자신을 그림.

--- src/w09/select_scene.py
+++ src/w09/select_scene.py
@@ -25,12 +25,23 @@ themes = [
     # }
 ]
 
+class Button(Sprite):
+    def __init__(self, nine_patch, title, x, y, width, height):
+        super().__init__(None, x, y)
+        self.bg = nine_patch
+        self.width, self.height = width, height
+        self.title = title
+        self.font = gfw.font.load('res/ENCR10B.TTF', 30)
+
+    def draw(self):
+        self.bg.draw(self.x, self.y, self.width, self.height)
+        gfw.font.draw_centered_text(self.font, self.title, self.x, self.y)
+
 def enter():
     world.append(Background('res/bg.png'), 0)
-    world.append(self, 1)
 
-    global font
-    font = gfw.font.load('res/ENCR10B.TTF', 30)
+    # global font
+    # font = gfw.font.load('res/ENCR10B.TTF', 30)
 
     global nine_patch
     nine_patch = gfw.image.NinePatch(gfw.image.load('res/round_rect_9.png'), 24, 24, 24, 24)
@@ -40,17 +51,16 @@ def enter():
     with open('res/themes.json', 'r') as f:
         themes = json.load(f)
 
+    y = start_y
+    for theme in themes:
+        world.append(Button(nine_patch, theme["title"], center_x, y, 400, 80), 1)
+        y -= 100
+
 def exit():
     world.clear()
 
 def update(): pass
-def draw(): 
-    y = start_y
-    for theme in themes:
-        nine_patch.draw(center_x, y, 400, 80)
-        gfw.font.draw_centered_text(font, theme["title"], center_x, y)
-        y -= 100
-
+def draw(): pass
 def handle_event(e): pass
 def pause(): pass
 def resume(): pass
