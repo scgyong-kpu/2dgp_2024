1b6062cb7c23b79c0c94de2bc804de00bf30425d
Kiyong Kim <scgyong@tukorea.ac.kr>
2024-11-06 12:14:40 +0900

    Highscore 를 임시로 만들어서 보여주기.

--- src/w09/highscore_scene.py
+++ src/w09/highscore_scene.py
@@ -1,16 +1,36 @@
 from pico2d import * 
 from gfw import *
+import pickle
+
+FILENAME = 'score.pickle'
 
 world = World(2)
 
 import sys
 self = sys.modules[__name__]
 
+class Entry:
+    def __init__(self, score):
+        self.score = score
+        self.time = time.localtime()
+    def timestr(self):
+        return time.strftime('%Y-%m-%d %H:%M:%S', self.time)
+
+
+try:
+    f = open(FILENAME, "rb")
+    scores = pickle.load(f)
+    f.close()
+    print("Scores:", scores)
+except:
+    print("No highscore file")
+    scores = [ Entry(score) for score in range(100, 200, 10) ]
+
 def enter():
     global frame_9p
     frame_9p = gfw.image.NinePatch(gfw.image.load('res/hs_frame.png'), 30, 30, 30, 30)
     global font
-    font = gfw.font.load('res/ENCR10B.TTF', 30)
+    font = gfw.font.load('res/ENCR10B.TTF', 25)
     world.append(self, 1)
 
 def exit():
@@ -22,6 +42,11 @@ def update():
 def draw():
     cw, ch = get_canvas_width(), get_canvas_height()
     frame_9p.draw(cw // 2, ch // 2, cw - 100, ch - 100)
+    x, y = 150, ch - 120
+    for score in scores:
+        font.draw(x, y, f'{score.score:5d}')
+        font.draw(x + 200, y, score.timestr())
+        y -= 40
 
 def pause():
     print('[main.pause()]')
