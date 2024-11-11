9b075a12c88620abb87b0bf63451ab4644bc10b7
Kiyong Kim <scgyong@tukorea.ac.kr>
2024-11-11 22:45:58 +0900

    board 를 module singleton 으로 하든지, Board class 로 하든지 결과는 같다.

--- src/w10-2048/board.py
+++ src/w10-2048/board.py
@@ -1,27 +1,30 @@
 
-blocks = [ [ None for _ in range(4) ] for _ in range(4) ]
+class Board:
+    def __init__(self):
+        self.blocks = [ [ None for _ in range(4) ] for _ in range(4) ]
 
-def get_block(x, y):
-    return blocks[y][x]
+    def get_block(self, x, y):
+        return self.blocks[y][x]
 
-def set_block(x, y, block):
-    blocks[y][x] = block
+    def set_block(self, x, y, block):
+        self.blocks[y][x] = block
 
-def print_blocks():
-    for y in range(3, -1, -1):
-        line = ''
-        for x in range(4):
-            b = get_block(x, y)
-            txt = '[      ] ' if b is None else f'[{b:^6d}] '
-            line += txt
-        print(line)
+    def print_blocks(self):
+        for y in range(3, -1, -1):
+            line = ''
+            for x in range(4):
+                b = self.get_block(x, y)
+                txt = '[      ] ' if b is None else f'[{b:^6d}] '
+                line += txt
+            print(line)
 
 def test_board():
-    set_block(2, 3, 16)
-    set_block(1, 2, 4)
-    set_block(0, 2, 8)
-    set_block(3, 0, 32)
-    print_blocks()
+    board = Board()
+    board.set_block(2, 3, 16)
+    board.set_block(1, 2, 4)
+    board.set_block(0, 2, 8)
+    board.set_block(3, 0, 32)
+    board.print_blocks()
 
 if __name__ == '__main__':
     test_board()
