5c80284216f43510eefb7109de5f0670f5469fa4
Kiyong Kim <scgyong@tukorea.ac.kr>
2024-11-11 23:32:02 +0900

    Spacebar Key 가 눌리면 생성되게 해 본다. Board 는 data 만 가지고 있다.

--- src/w10-2048/game_scene.py
+++ src/w10-2048/game_scene.py
@@ -1,5 +1,6 @@
 from pico2d import * 
 from gfw import *
+from board import Board
 
 world = World(['bg', 'block', 'ui'])
 
@@ -8,9 +9,18 @@ canvas_height = 600
 shows_bounding_box = True
 shows_object_count = True
 
+def generate_block():
+    gen = board.generate_block()
+    if gen is None: return
+
+    x, y, n = gen
+    print(f'{x=} {y=} {n=}')
+
 def enter():
     world.append(Background('res/FF9F49.png'), world.layer.bg)
-    pass
+
+    global board
+    board = Board()
 
 def exit():
     world.clear()
@@ -25,6 +35,9 @@ def handle_event(e):
     if e.type == SDL_KEYDOWN and e.key == SDLK_1:
         print(world.objects)
         return
+
+    if e.type == SDL_KEYDOWN and e.key == SDLK_SPACE:
+        generate_block()
 
 if __name__ == '__main__':
     gfw.start_main_module()
