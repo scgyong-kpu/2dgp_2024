040ee792e9473a80049468d172b0c2d46f70e3da
Kiyong Kim <scgyong@tukorea.ac.kr>
2024-11-11 22:41:26 +0900

    added board

---
+++ src/w10-2048/board.py
@@ -0,0 +1,28 @@
+
+blocks = [ None for _ in range(16) ]
+
+def get_block(x, y):
+    return blocks[y * 4 + x]
+
+def set_block(x, y, block):
+    blocks[y * 4 + x] = block
+
+def print_blocks():
+    for y in range(3, -1, -1):
+        line = ''
+        for x in range(4):
+            b = get_block(x, y)
+            txt = '[      ] ' if b is None else f'[{b:^6d}] '
+            line += txt
+        print(line)
+
+def test_board():
+    set_block(2, 3, 16)
+    set_block(1, 2, 4)
+    set_block(0, 2, 8)
+    set_block(3, 0, 32)
+    print_blocks()
+
+if __name__ == '__main__':
+    test_board()
+
