fdc6723d2eea0eb66add8923e27441cede380ea4
Kiyong Kim <scgyong@tukorea.ac.kr>
2024-11-12 00:23:51 +0900

    Move Animation

--- src/w10-2048/game_scene.py
+++ src/w10-2048/game_scene.py
@@ -11,14 +11,38 @@ shows_bounding_box = True
 shows_object_count = True
 
 class NumBlock(AnimSprite):
+    SPEED_PPS = 3000
     def __init__(self, n):
         fn = f'res/block_{n:05d}.png'
         super().__init__(fn, 0, 0, 10) # 10fps
         self.layer_index = world.layer.block
 
-    def move_to(self, x, y):
-        self.x = x * 120 + 80
-        self.y = y * 120 + 80
+    def move_to(self, x, y, animates=True):
+        tx = x * 120 + 80
+        ty = y * 120 + 80
+        if animates:
+            self.tx, self.ty = tx, ty
+        else:
+            self.x, self.y = tx, ty
+            self.tx, self.ty = None, None
+
+    def update(self):
+        if self.tx is None: return
+        dist = self.SPEED_PPS * gfw.frame_time
+        x, y = self.x, self.y
+        tx, ty = self.tx, self.ty
+        if x < tx:
+            x = min(x + dist, tx)
+        elif x > tx:
+            x = max(tx, x - dist)
+        if y < ty:
+            y = min(y + dist, ty)
+        elif y > ty:
+            y = max(ty, y - dist)
+
+        self.x, self.y = x, y
+        if (self.x, self.y) == (self.tx, self.ty):
+            self.tx, self.ty = None, None
 
     def remove(self):
         world.remove(self)
@@ -31,7 +55,7 @@ def generate_block():
 
     block = NumBlock(random.choice([2, 4]))
     x, y = board.generate_block(block)
-    block.move_to(x, y)
+    block.move_to(x, y, False)
 
     world.append(block)
 
