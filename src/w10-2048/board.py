26ae195324d28821ac8b1cf6a3052eac6602fc3f
Kiyong Kim <scgyong@tukorea.ac.kr>
2024-11-11 22:48:29 +0900

    상수(?) 정의 이용.

--- src/w10-2048/board.py
+++ src/w10-2048/board.py
@@ -1,7 +1,10 @@
 
+CX_BLOCK = 4
+CY_BLOCK = 4
+
 class Board:
     def __init__(self):
-        self.blocks = [ [ None for _ in range(4) ] for _ in range(4) ]
+        self.blocks = [ [ None for _ in range(CX_BLOCK) ] for _ in range(CY_BLOCK) ]
 
     def get_block(self, x, y):
         return self.blocks[y][x]
@@ -10,9 +13,9 @@ class Board:
         self.blocks[y][x] = block
 
     def print_blocks(self):
-        for y in range(3, -1, -1):
+        for y in range(CY_BLOCK - 1, -1, -1):
             line = ''
-            for x in range(4):
+            for x in range(CX_BLOCK):
                 b = self.get_block(x, y)
                 txt = '[      ] ' if b is None else f'[{b:^6d}] '
                 line += txt
