201c7aca1d00cfd54a2c98b3ef58d0bf7141a83f
Kiyong Kim <scgyong@tukorea.ac.kr>
2024-11-15 20:38:22 +0900

    quicktype.io 에서 만든 파일은 editorsettings 가 mandatory 인 것으로 판단하고 있다

--- src/w11-w06-DF/map_bg.py
+++ src/w11-w06-DF/map_bg.py
@@ -13,4 +13,19 @@ class MapBg:
         print(self.tmap.tilesets[0])
 
 if __name__ == '__main__':
-    mapbg = MapBg('res/earth.json')
+    mapbg = MapBg('res/desert.json')
+
+'''
+Traceback (most recent call last):
+  File "D:/Lectures/2024_2/2dgp/git/src/w11-w06-DF/map_bg.py", line 16, in <module>
+    mapbg = MapBg('res/desert.json')
+  File "D:/Lectures/2024_2/2dgp/git/src/w11-w06-DF/map_bg.py", line 8, in __init__
+    self.tmap = tiledmap.tiled_map_from_dict(mapjson)
+  File "D:/Lectures/2024_2/2dgp/git/src/w11-w06-DF/tiledmap.py", line 270, in tiled_map_from_dict
+    return TiledMap.from_dict(s)
+  File "D:/Lectures/2024_2/2dgp/git/src/w11-w06-DF/tiledmap.py", line 231, in from_dict
+    editorsettings = Editorsettings.from_dict(obj.get("editorsettings"))
+  File "D:/Lectures/2024_2/2dgp/git/src/w11-w06-DF/tiledmap.py", line 65, in from_dict
+    assert isinstance(obj, dict)
+AssertionError
+'''
