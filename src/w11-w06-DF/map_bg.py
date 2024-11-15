818b20c5d6018f20d7c7ab4f23172b9b64863450
Kiyong Kim <scgyong@tukorea.ac.kr>
2024-11-15 21:15:53 +0900

    MapBg 를 world 에 추가하고 update/draw 추가.

--- src/w11-w06-DF/map_bg.py
+++ src/w11-w06-DF/map_bg.py
@@ -16,10 +16,15 @@ class MapBg:
         for ts in self.tmap.tilesets:
             ts.tile_image = gfw.image.load(f'{self.folder}/{ts.image}')
             print(ts.tile_image)
+    def update(self): pass
+    def draw(self):
+        pass
 
 class TestScene:
     def enter(self):
         self.world = World()
+        self.map_bg = MapBg('res/earth.json')
+        self.world.append(self.map_bg, 0)
     def exit(self): pass
     def handle_event(self, e): pass
 
