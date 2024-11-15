d5d8d26e73ad3d231e3c1d57cdba6fc811d25c04
Kiyong Kim <scgyong@tukorea.ac.kr>
2024-11-15 20:27:43 +0900

    https://app.quicktype.io/ 에서 JSON Parser 를 생성. map_bg.py 를 실행시켜 본다.

---
+++ src/w11-w06-DF/map_bg.py
@@ -0,0 +1,16 @@
+import json
+import tiledmap
+
+class MapBg:
+    def __init__(self, filename):
+        with open(filename, 'r') as f:
+            mapjson = json.load(f)
+        self.tmap = tiledmap.tiled_map_from_dict(mapjson)
+        # print(self.tmap)
+        # print(self.tmap.layers)
+        # print(self.tmap.layers[0])
+        print(self.tmap.layers[0].data)
+        print(self.tmap.tilesets[0])
+
+if __name__ == '__main__':
+    mapbg = MapBg('res/earth.json')
