15430c447367dad06a687bdf5a7ef959457838f0
Kiyong Kim <scgyong@tukorea.ac.kr>
2024-11-15 21:09:46 +0900

    json 파일의 폴더를 얻고 tileset 의 이미지까지 로드해본다.

--- src/w11-w06-DF/map_bg.py
+++ src/w11-w06-DF/map_bg.py
@@ -1,20 +1,25 @@
 import json
 import tiledmap
+from gfw import *
+
+def get_folder(filename):
+    idx = filename.rfind('/')
+    return '.' if idx < 0 else filename[:idx]
 
 class MapBg:
     def __init__(self, filename):
+        self.map_filename = filename
+        self.folder = get_folder(filename)
         with open(filename, 'r') as f:
             mapjson = json.load(f)
         self.tmap = tiledmap.tiled_map_from_dict(mapjson)
-        # print(self.tmap)
-        # print(self.tmap.layers)
-        # print(self.tmap.layers[0])
-        print(self.tmap.layers[0].data)
-        print(self.tmap.tilesets[0])
+        for ts in self.tmap.tilesets:
+            ts.tile_image = gfw.image.load(f'{self.folder}/{ts.image}')
+            print(ts.tile_image)
 
 if __name__ == '__main__':
+    open_canvas()
     mapbg = MapBg('res/earth.json')
-    print('-' * 60)
-    mapbg = MapBg('res/desert.json')
+    close_canvas()
 
 
