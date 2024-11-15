1edc3c525af11143e80f6342ee8d5976e41d3712
Kiyong Kim <scgyong@tukorea.ac.kr>
2024-11-15 21:57:47 +0900

    타일 한 장을 그리는 함수를 만들어 부른다.

--- src/w11-w06-DF/map_bg.py
+++ src/w11-w06-DF/map_bg.py
@@ -23,9 +23,12 @@ class MapBg:
         for layer in self.tmap.layers:
             self.draw_layer(layer)
     def draw_layer(self, layer):
+        tx, ty = 0, 0 # 0,0 위치의 타일을 그리겠다. map 의 0,0 은 좌상단이다.
+        dst_left, dst_top = 0, get_canvas_height()
+        self.draw_tile(layer, tx, ty, dst_left, dst_top)
+    def draw_tile(self, layer, tx, ty, dst_left, dst_top):
         tileset = self.tmap.tilesets[0] # first tileset only
         rows = math.ceil(tileset.tilecount / tileset.columns) # 타일셋의 세로방향 타일 갯수를 구한다. 
-        tx, ty = 0, 0 # 0,0 위치의 타일을 그리겠다. map 의 0,0 은 좌상단이다.
         t_index = ty * layer.width + tx # 그 위치의 타일정보는 data[t_index] 에 있다
         tile = layer.data[t_index]   # 그려야 할 타일 번호를 구한다
         sx = (tile - 1) % tileset.columns  # 해당 번호의 타일은 타일셋이미지 에서 왼쪽으로부터 sx 번째에 있다
@@ -33,7 +36,7 @@ class MapBg:
         src_left = tileset.margin + sx * (tileset.tilewidth + tileset.spacing) # 타일은 이미지의 src_left 번째 픽셀부터 시작 = 소스 x 좌표
         src_botm = tileset.margin + (rows - sy - 1) * (tileset.tileheight + tileset.spacing) # 소스 y 좌표.
 
-        dst_left, dst_botm = 0, get_canvas_height() - self.tilesize
+        dst_botm = dst_top - self.tilesize
         tileset.tile_image.clip_draw_to_origin(
             src_left, src_botm, tileset.tilewidth, tileset.tileheight, 
             dst_left, dst_botm, self.tilesize, self.tilesize)
