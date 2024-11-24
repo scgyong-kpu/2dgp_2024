390b9e923b6ca16d4a611c4199bd4ece22a6974a
Kiyong Kim <scgyong@tukorea.ac.kr>
2024-11-25 03:29:17 +0900

    tilesize 를 줄인다면

--- src/w12-map-world/main_scene.py
+++ src/w12-map-world/main_scene.py
@@ -10,7 +10,7 @@ shows_bounding_box = True
 shows_object_count = True
 
 def enter():
-    world.bg = MapBackground('res/desert.tmj')
+    world.bg = MapBackground('res/desert.tmj', tilesize=20)
     world.bg.margin = 100
     world.bg.set_collision_tiles({1,2,3,9,10,11,17,18,19,20,21,25,26,27,28,29,33,34,35,36,37,41,42,43,44,45})
     world.append(world.bg, world.layer.bg)
