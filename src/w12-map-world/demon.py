becdc0cf08211fc028925d8b63bbe0ac7fbd662d
Kiyong Kim <scgyong@tukorea.ac.kr>
2024-11-25 21:42:24 +0900

    Demon 이 Player 를 따라가도록.

--- src/w12-map-world/demon.py
+++ src/w12-map-world/demon.py
@@ -1,3 +1,4 @@
+import math
 from pico2d import * 
 from gfw import *
 
@@ -5,8 +6,16 @@ class Demon(AnimSprite):
     def __init__(self, x, y):
         super().__init__('res/demon_itsumade.png', x, y, 10)
         self.layer_index = gfw.top().world.layer.enemy
+        self.speed = 100
         self.bb_width = 30
         self.bb_height = 30
+
+    def update(self):
+        world = gfw.top().world
+        player = world.object_at(world.layer.player, 0)
+        angle_radian = math.atan2(player.y - self.y, player.x - self.x)
+        self.x += self.speed * math.cos(angle_radian) * gfw.frame_time
+        self.y += self.speed * math.sin(angle_radian) * gfw.frame_time
 
     def get_bb(self):
         l = self.x - self.bb_width // 2
