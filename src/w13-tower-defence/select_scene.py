3f79a3c7b582a92dc2326bbc690ce964c085b4a4
Kiyong Kim <scgyong@tukorea.ac.kr>
2024-11-30 22:27:04 +0900

    select scene 생성.

---
+++ src/w13-tower-defence/select_scene.py
@@ -0,0 +1,35 @@
+from pico2d import * 
+from gfw import *
+import game_scene
+
+canvas_width = game_scene.canvas_width
+canvas_height = game_scene.canvas_height
+
+import sys
+self = sys.modules[__name__]
+
+world = gfw.World(['bg', 'button'])
+
+def enter():
+    world.append(self, world.layer.bg)
+    global tile_img
+    tile_img = gfw.image.load('res/select_bg_tile.png')
+def exit():
+    world.clear()
+def update(): pass
+def draw():
+    tilesize = 48
+    for y in range(0, canvas_height, tilesize):
+        for x in range(0, canvas_width, tilesize):
+            tile_img.draw_to_origin(x, y, tilesize, tilesize)
+def pause():
+    print('[select.pause()]')
+def resume():
+    print('[select.resume()]')
+def handle_event(e):
+    pass
+
+if __name__ == '__main__':
+    gfw.start_main_module()
+
+
