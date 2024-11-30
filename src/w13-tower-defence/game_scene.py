86ad0a3cb07c3857c1873acbb46af407c3448a15
Kiyong Kim <scgyong@tukorea.ac.kr>
2024-11-30 23:52:59 +0900

    fly 추가

--- src/w13-tower-defence/game_scene.py
+++ src/w13-tower-defence/game_scene.py
@@ -1,12 +1,13 @@
 from pico2d import * 
 from gfw import *
 import stage_path
+import fly_gen
 
 canvas_width = 1536 # 32 * 48
 canvas_height = 864 # 18 * 48
 shows_object_count = True
 
-world = gfw.World(['bg', 'path'])
+world = gfw.World(['bg', 'path', 'fly', 'controller'])
 
 stage = 1
 
@@ -21,6 +22,10 @@ def enter():
     stage_path.set_tile_bg(bg)
     map_path = stage_path.path_shower()
     world.append(map_path, world.layer.path)
+
+    fly_gen.init()
+    world.append(fly_gen, world.layer.controller)
+
 
 def exit():
     world.clear()
