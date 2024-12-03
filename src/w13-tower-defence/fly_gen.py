9a812cdbc8d58ef5f60defabdb7e2d10854afe1a
KKY@macbook <scgyong@tukorea.ac.kr>
2024-12-03 11:40:49 +0900

    stage_path 의 목적지를 따라 이동.

--- src/w13-tower-defence/fly_gen.py
+++ src/w13-tower-defence/fly_gen.py
@@ -1,18 +1,44 @@
 from pico2d import * 
 from gfw import *
-# import stage_path
+import stage_path
 import random
 
 GEN_INTERVAL = 1.0
 class Fly(AnimSprite):
     def __init__(self, type):
-        x = random.randint(100, get_canvas_width() - 100)
-        y = random.randint(100, get_canvas_height() - 100)
+        # x = random.randint(100, get_canvas_width() - 100)
+        # y = random.randint(100, get_canvas_height() - 100)
+        x, y = stage_path.path_coords[0] # spawn position
         super().__init__(f'res/fly_{type}.png', x, y, 3)
         self.layer_index = world.layer.fly
+        self.speed = 100
+        self.path_index = 1
+        self.set_target_position()
 
-    # def update(self):
-    #     self.x += 100 * gfw.frame_time
+    def set_target_position(self):
+        if self.path_index >= len(stage_path.path_coords):
+            self.dx, self.dy = 0, 0
+            return
+        self.tx, self.ty = stage_path.path_coords[self.path_index]
+        # print(f'{(self.tx, self.ty)=}')
+        dx, dy = self.tx - self.x, self.ty - self.y
+        dist = math.sqrt(dx ** 2 + dy ** 2)
+        self.dx, self.dy = dx / dist, dy / dist
+        # print(f'{(self.dx, self.dy)=}')
+
+    def update(self):
+        self.x += self.dx * self.speed * gfw.frame_time
+        self.y += self.dy * self.speed * gfw.frame_time
+        dx, dy = self.tx - self.x, self.ty - self.y
+        if dx < -1 or 1 < dx or dy < -1 or 1 < dy:
+            # print(f'{(dx, dy)=}')
+            return
+        dist = math.sqrt(dx ** 2 + dy ** 2)
+        # print(dist)
+        if dist < 1:
+            self.path_index += 1
+            self.set_target_position()
+        # print(f'({self.dx:.1f}, {self.dy:.1f}) ({self.x=:.1f}, {self.y=:.1f}) {self.speed=} * {gfw.frame_time=:.2f} {self.dx * self.speed * gfw.frame_time}')
 
 
 # module itself as Fly Generator
