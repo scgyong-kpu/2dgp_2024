b7b81c7c68f92af3c9209c3cc28ef525cc605233
KKY@macbook <scgyong@tukorea.ac.kr>
2024-12-03 12:03:47 +0900

    ESC 를 1초 이내에 누르면 Exit 하도록.

--- src/w13-tower-defence/pause_scene.py
+++ src/w13-tower-defence/pause_scene.py
@@ -7,7 +7,14 @@ self = sys.modules[__name__]
 world = World(2)
 transparent = True
 
+EXIT_TIMEOUT = 1.0
+MSG_EXIT = 'Press ESC to Exit the game'
+MSG_RESUME = 'Press ESC to Resume the game'
+
 def enter():
+    global time, msg
+    time, msg = EXIT_TIMEOUT, MSG_EXIT
+
     global center_x, center_y
     center_x = get_canvas_width() // 2
     center_y = get_canvas_height() // 2
@@ -24,17 +31,23 @@ def exit():
     world.clear()
 
 def handle_event(e):
-    if e.type == SDL_KEYDOWN and e.key == SDLK_BACKSPACE:
-        gfw.pop()
+    if e.type == SDL_KEYDOWN and e.key == SDLK_ESCAPE:
+        if time > 0:
+            gfw.pop()
         gfw.pop()
         return True
 
 def draw():
-    gfw.font.draw_centered_text(font, 'Press ESC to Resume',     center_x, center_y + 30, (63, 0, 0))
-    gfw.font.draw_centered_text(font, 'Press Backspace to Exit', center_x, center_y - 20, (0, 0, 63))
+    gfw.font.draw_centered_text(font, msg, center_x, center_y + 30, (63, 0, 0))
+    # gfw.font.draw_centered_text(font, 'Press Backspace to Exit', center_x, center_y - 20, (0, 0, 63))
 
 def update():
-    pass
+    global time, msg
+    if time > 0:
+        time -= gfw.frame_time
+        if time <= 0:
+            msg = MSG_RESUME
+
 
 if __name__ == '__main__':
     gfw.start_main_module()
