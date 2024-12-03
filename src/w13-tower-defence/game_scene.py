39f42a540caa9dfd7dc1f77148d873a7ddfcc80e
KKY@macbook <scgyong@tukorea.ac.kr>
2024-12-03 10:25:58 +0900

    path 는 stage_path module 이 관리하도록 한다.

--- src/w13-tower-defence/game_scene.py
+++ src/w13-tower-defence/game_scene.py
@@ -1,5 +1,6 @@
 from pico2d import * 
 from gfw import *
+import stage_path
 
 tilesize = 48
 canvas_width = 32 * tilesize # 1536 = 32 * 48
@@ -13,37 +14,15 @@ stage = 1
 # stage = 2
 # stage = 3
 
-class PathDraw:
-    def __init__(self, bg):
-        self.bg = bg
-        self.image = gfw.image.load('res/trans_50b.png')
-        self.path_tiles = []
-
-        print(self.path_tiles)
-    def update(self): pass
-
-    def draw(self):
-        size = self.bg.tilesize
-        for tx, ty in self.path_tiles:
-            x, y = self.bg.to_screen(tx * size, ty * size)
-            self.image.draw_to_origin(x, y, size, size)
-
 
 def enter():
     global bg
     bg = MapBackground(f'res/map/stage_{stage:02d}.json', tilesize=tilesize, wraps=False)
-    layer = bg.tmap.layers[1]
-
-    ts = bg.tmap.tileheight
-    mh = bg.tmap.height
-    start_pos, end_pos = map(lambda o: (int(o['x'] // ts), mh - int(o['y'] // ts) - 1), layer.objects[0:2])
-
-    print(f'{start_pos=}, {end_pos=:}')
-
-    pd = PathDraw(bg)
-    pd.path_tiles = [ start_pos, end_pos ]
     world.append(bg, world.layer.bg)
-    world.append(pd, world.layer.path)
+
+    stage_path.set_tile_bg(bg)
+    map_path = stage_path.path_shower()
+    world.append(map_path, world.layer.path)
 
 def exit():
     world.clear()
---
+++ src/w13-tower-defence/stage_path.py
@@ -0,0 +1,32 @@
+import gfw
+
+class PathDraw:
+    def __init__(self, bg, tiles=[]):
+        self.bg = bg
+        self.image = gfw.image.load('res/trans_50b.png')
+        self.path_tiles = tiles
+
+        # print(self.path_tiles)
+    def update(self): pass
+
+    def draw(self):
+        size = self.bg.tilesize
+        for tx, ty in self.path_tiles:
+            x, y = self.bg.to_screen(tx * size, ty * size)
+            self.image.draw_to_origin(x, y, size, size)
+
+def set_tile_bg(bg):
+    global map_bg
+    map_bg = bg
+
+    layer = bg.tmap.layers[1]
+
+    ts = bg.tmap.tileheight
+    mh = bg.tmap.height
+    start_pos, end_pos = map(lambda o: (int(o['x'] // ts), mh - int(o['y'] // ts) - 1), layer.objects[0:2])
+
+    global path_tiles
+    path_tiles = [ start_pos, end_pos ]
+
+def path_shower():
+    return PathDraw(map_bg, path_tiles)
