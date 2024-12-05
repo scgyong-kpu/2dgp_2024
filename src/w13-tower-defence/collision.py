d5f2ffbbd91c98905426d1acc8901839d031acc3
Kiyong Kim <scgyong@tukorea.ac.kr>
2024-12-05 22:41:13 +0900

    bullet 을 바깥 루프로 돌림.

--- src/w13-tower-defence/collision.py
+++ src/w13-tower-defence/collision.py
@@ -5,8 +5,8 @@ def draw(): pass
 
 def update():
     world = gfw.top().world
-    for e in world.objects_at(world.layer.fly):
-        for b in world.objects_at(world.layer.bullet):
+    for b in world.objects_at(world.layer.bullet):
+        for e in world.objects_at(world.layer.fly):
             if collides_box(e, b):
                 if e.hit(b.power):
                     world.remove(e)
